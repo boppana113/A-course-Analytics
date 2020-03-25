73a9985c7b5abd58a10f99a3f551118e5fc4fced
Sriram Boppana <boppana113@gmail.com>
2020-03-25 19:30:54 -0400

    deleted rows with NAN

--- analytic.R
+++ analytic.R
@@ -1,2 +1,9 @@
 gender = read.csv("gender_submission.csv",header = TRUE)
-train_data = read.csv("train.csv",header = True)
+data = read.csv("train.csv",header = TRUE)
+
+# Remove Rows with incomplete cases 
+data[complete.cases(data), ] # Keep only the complete rows
+data_complete <- data[complete.cases(data), ] # Store the complete cases subset in a new data frame
+
+summary(data_complete)
+write.csv(data_complete,"train_complete.csv")
