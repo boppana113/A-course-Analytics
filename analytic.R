863ca7cd8c5d420d374a78380abe4ccb1f4f42c0
Boppana, Sriram (Sriram) <sriram.boppana@ge.com>
2020-03-26 09:07:17 -0400

    Added one more plot

--- analytic.R
+++ analytic.R
@@ -3,19 +3,20 @@ library(corrplot)
 library(dplyr)
 library(ggplot2)
 
-R# Load datasets
+# Load datasets
 gender = read.csv("gender_submission.csv",header = TRUE)
 data = read.csv("train.csv",header = TRUE)
 
 # Remove Rows with incomplete cases 
 data <- na.omit(data)
-data <- subset(data, select = -c(1,4,9))
+data <- subset(data, select = -c(1,4,9)) #Removed ID, Name, and Ticket number 
 write.csv(data,"train_filtered.csv")
 dim(data)
 
 hist(data[data$Survived==1,]$Age, col="lightgreen", xlab="Age",main = "Survived by Age")
 hist(data[data$Survived==0,]$Age, col="firebrick2", xlab="Age",main = "Died by Age")
 
+pairs(data)
 
 # Bifurcate into those who survived and those who died
 # data_lived <- subset(data, Survived==1)
@@ -34,4 +35,16 @@ ggplot(data_lived,aes(x=Sex)) +
   geom_bar()
 
 ggplot(data_died,aes(x=Sex)) +
+  geom_bar()
+
+ggplot(data_lived,aes(x=Pclass)) +
+  geom_bar()
+
+ggplot(data_died,aes(x=Pclass)) +
+  geom_bar()
+
+ggplot(data_lived,aes(x=Embarked)) +
+  geom_bar()
+
+ggplot(data_died,aes(x=Embarked)) +
   geom_bar()
